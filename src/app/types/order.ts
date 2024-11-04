d4aac7238176cd73c5b31ef96393aa7af9981444
jonas <jonas.pfeifer@htlstp.at>
2024-11-04 15:50:03 +0100

    init

--- src/app/types/order.ts
+++ src/app/types/order.ts
@@ -1,7 +1,7 @@
 import {ProductEntry} from "./productEntry";
 
 export type Order = {
-  status: "in_progress" | "done";
+  status: "in_progress" | "ready" | "finished";
   uuid: string;
   entries: ProductEntry[]
 }
